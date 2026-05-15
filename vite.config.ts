59f28583bd61c4d367501781720f8ba2b02f9df5
ASRathore0 <asingh68416@gmail.com>
2026-05-15 19:37:47 +0530

    fix production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+export default defineConfig({
+  base: '/Thrive/',
 
-  return {
-    base: '/Thrive/', // ADD THIS
+  plugins: [react(), tailwindcss()],
 
-    plugins: [react(), tailwindcss()],
-
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-      watch: process.env.DISABLE_HMR === 'true' ? null : {},
-    },
-  };
+  },
 });
